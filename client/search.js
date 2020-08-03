636942a7a20c3b4773046b8fcde4a4ccaa87ecc2
Mustapha Moshood <olawale3415@gmail.com>
2020-08-03 07:32:19 +0100

    connected frontend to backend

--- client/search.js
+++ client/search.js
@@ -9,19 +9,21 @@ const formClick = click.addEventListener("click", async (event) => {
 
 const Search = async (name) => {
   axios
-    .post("http://localhost:8080/search", name, { headers: headers})
+    .post("https://pen-image.herokuapp.com/search", name, { headers: headers})
     .then((response) => {
       response.data; 
       if (response.status == "200") {
         if ( typeof(response.data.error) == "undefined") {
-          console.log(response.data.output)
-          // const preview = document.querySelector(".prevbox");
-          // let image_img = document.createElement('img');
-          // image_img.classList.add('image_img');
-          // image_img.src = response.data.picture;
-          // image_img.alt = response.data.name;
-          // preview.appendChild(image_img);
-
+          const imageOne = document.querySelector("#one");
+          imageOne.src = response.data.output[0]
+          const imageTwo = document.querySelector("#two");
+          imageTwo.src = response.data.output[1]
+          const imageThree = document.querySelector("#three");
+          imageThree.src = response.data.output[2]
+          const imageFour = document.querySelector("#four");
+          imageFour.src = response.data.output[3]
+          const imageFive = document.querySelector("#five");
+          imageFive.src = response.data.output[4]
         } else { 
           show("Unable to search.", "error");
         }
